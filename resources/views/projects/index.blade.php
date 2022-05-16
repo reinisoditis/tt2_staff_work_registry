2aaec1707c388b1bd3d98b38277fe20641367e1e
reinisoditis <reinis@oduzeme.net>
2022-05-16 23:11:34 +0300

    first lists created

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,35 +10,45 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-col">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Name
-                                </th>
-                                <th scope="col" class="relative px-6 py-3">
-                                    <span class="sr-only">Edit</span>
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($projects as $project)
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $project->name }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a href="{{ route('projects.index', $project) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-
-                    </div>
-                    <div class="mt-4">
-                    {{ $projects->links() }}
-                    </div>
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full w-full">
+                                        <thead class="bg-gray-50 table-fixed">
+                                        <tr>
+                                            <th scope="col" class="border border-slate-600 text-lg font-medium text-gray-500 uppercase ">
+                                                Name
+                                            </th>
+                                            <th scope="col" class="border border-slate-600 text-lg font-medium text-gray-500 uppercase">
+                                                <span class="sr-only">Edit</span>
+                                            </th>
+                                            <th scope="col" class="border border-slate-600 text-lg font-medium text-gray-500 uppercase">
+                                                <span class="sr-only">Delete</span>
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach ($projects as $project)
+                                        <tr>
+                                            <td class="border border-slate-600 px-6 py-4 whitespace-nowrap text-sm text-gray-500 uppercase">
+                                                {{ $project->name }}
+                                            </td>
+                                            <td  class="border border-slate-600 px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                                <button type="button" onclick="{{ route('projects.index', $project) }}" class="text-indigo-600 hover:text-indigo-900">Edit</button>
+                                            </td>
+                                            <td  class="border border-slate-600 px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                                <button type="button" onclick="{{ route('projects.index', $project) }}" class="text-indigo-600 hover:text-indigo-900">Delete</button>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="mt-4">
+                                {{ $projects->links() }}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
